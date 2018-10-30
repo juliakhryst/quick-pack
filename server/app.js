31014a74962429ab5128049a90527a85d99d9596
Julia Khryst <juleczkacezar@gmail.com>
2018-10-30 02:19:23 +0200

    Change server clientPath

--- server/app.js
+++ server/app.js
@@ -2,14 +2,17 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const clientPath = path.resolve(__dirname, '../dist/quick-pack');
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/quick-pack'));
+app.use(express.static(clientPath));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/quick-pack/index.html'));
+app.get('/*', function(req, res) {
+    res.sendFile(path.resolve(__dirname, clientPath, '/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+const server = app.listen(process.env.PORT || 8080, function() {
+    const port = server.address().port;
+    console.log('The server is running on port: ' + port);
+});

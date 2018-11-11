572774f2af8b1d27ef9aeabd628890ca387c2a68
Anatolii Dmytruk <tolikd1488@gmail.com>
2018-11-11 16:49:30 +0200

    Fixed translation service

--- src/app/language-switcher/language-switcher.component.ts
+++ src/app/language-switcher/language-switcher.component.ts
@@ -1,5 +1,5 @@
 import { TranslateService } from '@ngx-translate/core';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
 
 @Component({
   selector: 'qpac-language-switcher',
@@ -12,11 +12,14 @@ export class LanguageSwitcherComponent implements OnInit {
   constructor(public translate: TranslateService) {
     translate.addLangs(['Ua', 'En']);
     translate.setDefaultLang('Ua');
-    translate.use('Ua');
+
+    if (this.translate.currentLang === undefined) {
+      translate.use('Ua');
+    }
 
     this.selected = this.translate.currentLang;
+    translate.use(this.selected);
   }
-
 
   ngOnInit() {
   }

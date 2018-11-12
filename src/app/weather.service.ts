d4d6d8cb68201967fe1ce4a18866f3c8d785bfcb
Vladyslav Fang <Vladyslav_Fang@epam.com>
2018-11-13 01:17:28 +0200

    Revert the testing approaches.

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable()
 export class WeatherService {
@@ -12,8 +12,8 @@ export class WeatherService {
 
   // t= time, l = language;
   getWeather(t, l): Observable<any> {
-    this.url = this.domain + 'forecast/6c2092633e864891808d080ca5675d1d/52.520008,13.404954,' + t + 'T12:00:00+02:00?units=si&lang=' + l;
-    const weatherResponse = this.http.get(this.url);
+    // this.url = this.domain + 'forecast/6c2092633e864891808d080ca5675d1d/52.520008,13.404954,' + t + 'T12:00:00+02:00?units=si&lang=' + l;
+    const weatherResponse = this.http.get(`http://localhost:8080/weather?t=${t}&l=${l}`);
 
     return weatherResponse.pipe(map(res => res));
   }

776e8614e4ea18aceb67a9e9704ea6b900c6c21b
Vladyslav Fang <Vladyslav_Fang@epam.com>
2018-11-13 01:14:59 +0200

    Test approaches.

--- server/app.js
+++ server/app.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const path = require('path');
+const request = require('request');
+const cors = require('cors')
 
 const app = express();
 const clientPath = path.resolve(__dirname, '../dist/quick-pack');
 
 // Serve only the static files form the dist directory
 app.use(express.static(clientPath));
+app.use(cors());
 
-app.get('/*', function(req, res) {
+const weatherDomain = 'https://api.darksky.net/';
+
+app.get('/weather', function(req, res) {
+    request.get(`${weatherDomain}forecast/6c2092633e864891808d080ca5675d1d/52.520008,13.404954,${req.query.t}T12:00:00+02:00?units=si&lang=${req.query.l}`, function (error, response, body) {
+        if (error) {
+            res.status(error.status).send(error);
+        }
+        
+        res.send(body);
+      })
+});
+
+app.all('/*', function(req, res) {
     res.sendFile(path.resolve(__dirname, clientPath, '/index.html'));
 });
+
+
 
 // Start the app by listening on the default Heroku port
 const server = app.listen(process.env.PORT || 8080, function() {

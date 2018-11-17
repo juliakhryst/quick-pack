c3ac8bc5f1bc44f01ed32a4f54ef4ae1676f1624
Anatolii Dmytruk <tolikd1488@gmail.com>
2018-11-17 17:45:57 +0200

    Mocked up some data

--- src/app/generation.service.ts
+++ src/app/generation.service.ts
@@ -18,8 +18,8 @@ export class GenerationService {
     } else if (activities === { baby: false, pet: false, sport: false }) {
       return this.item$ = this.afs.collection('list-items', ref => ref.where('activities', '==', 'Essential')).valueChanges();
     } else {
-      return this.item$ = this.afs.collection('list-items', ref => ref.where('weather', 'array-contains', weather)
-      .where('type', '==', type).where('activities', '==', activities)).valueChanges();
+      return this.item$ = this.afs.collection('list-items', ref => ref.where('weather', 'array-contains', 'cold'/*weather*/)
+      .where('type', '==', type)/*.where('activities', '==', activities)*/).valueChanges();
     }
   }
 

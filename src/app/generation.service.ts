757fce3048adc9bdf9428332c12c4f2efec69658
Anatolii Dmytruk <tolikd1488@gmail.com>
2018-11-18 16:38:15 +0200

    forkJoin

--- src/app/generation.service.ts
+++ src/app/generation.service.ts
@@ -1,6 +1,7 @@
 import { AngularFirestore } from 'angularfire2/firestore';
-import { Observable } from 'rxjs';
+import { Observable, forkJoin } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,25 +10,30 @@ export class GenerationService {
   items$: Observable<any>;
   essential$: Observable<any>;
   filteredItems$: Observable<any>;
-  prop1;
-  prop2;
+  respons;
 
   constructor(private afs: AngularFirestore) {
   }
 
   getList(weather, type, activities) {
-    this.essential$ = this.afs.collection('list-items', ref => ref.where('activities', '==', 'Essential')).valueChanges();
+    this.essential$ = this.afs.collection('pack-items', ref => ref.where('activities', '==', 'Essential')).valueChanges();
 
     if (activities === { baby: true, pet: true, sport: true }) {
-    return this.items$ = this.afs.collection('list-items', ref => ref.where('weather', 'array-contains', weather)
+    return this.items$ = this.afs.collection('pack-items', ref => ref.where('weather', 'array-contains', weather)
     .where('type', '==', type)).valueChanges();
     } else if (activities === { baby: false, pet: false, sport: false }) {
       this.items$ = this.essential$;
       return this.items$;
     } else {
-      this.filteredItems$ = this.afs.collection('list-items', ref => ref.where('weather', 'array-contains', 'average'/*weather*/)
+      this.filteredItems$ = this.afs.collection('pack-items', ref => ref.where('weather', 'array-contains', 'average'/*weather*/)
       .where('type', '==', type)/*.where('activities', '==', activities)*/).valueChanges();
-      this.items$ = this.filteredItems$;
+
+      this.items$ =  forkJoin(
+        this.essential$,
+        this.filteredItems$,
+      ).pipe(map((resp1, resp2) => this.respons));
+
+      // const subscribe = this.items$.subscribe(val => console.log(val));
 
       return this.items$;
     }
